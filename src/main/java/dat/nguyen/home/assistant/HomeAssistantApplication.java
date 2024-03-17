609d499bb81ba3637ec6a26aa2c175ee45d1de8b
datnguyen2k3 <thanhdatmu2003@gmail.com>
2024-03-17 14:13:03 +0700

    init

---
+++ src/main/java/dat/nguyen/home/assistant/HomeAssistantApplication.java
@@ -0,0 +1,13 @@
+package dat.nguyen.home.assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeAssistantApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HomeAssistantApplication.class, args);
+  }
+
+}
---
+++ src/test/java/dat/nguyen/home/assistant/HomeAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package dat.nguyen.home.assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeAssistantApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
